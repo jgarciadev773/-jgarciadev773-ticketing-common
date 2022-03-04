84e722a7477c9b7bf1280a82469a6b8e5e3416f2
Josh Garcia <jgarcia@psclistens.com>
2022-03-04 15:13:47 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+    red: number,
+    white: number,
+    blue: number
+};
+
+const color: Color = {
+    red: 4,
+    white: 1,
+    blue: 77
+};
+
+console.log(color);
+
+export default color;
